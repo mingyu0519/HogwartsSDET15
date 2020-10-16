5b6a6e80be4797d64b5fd5c0bf715a2956416ca6
mingyu.wang <mingyu.wang@ly.com>
2020-10-16 14:24:12 +0800

    提交测试用例

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pytest
-
 from pythoncode.calculator import Calculator
-
-
-def test_a():
-    print("test case a")
 
 
 class TestCalc:
@@ -18,19 +13,22 @@ class TestCalc:
         print("计算结束")
 
     @pytest.mark.parametrize('a,b,expect', [
-        [1, 1, 2], [100, 1, 200], [0.1, 0.1, 0.2], [-1, -1, -2],
-        [1, 0, 1]
-    ], ids=['int_case', 'bignum_case', 'float_case', 'minus_case', 'zero_case'])
+        [1, 1, 2], [100, 1, 101], [0.1, 0.1, 0.2], [-1, -1, -2],
+        [1, 0, 1], [0, 1, 1], [-1, 1, 0], [-100, -400, -500], [-500, 1000, 500]
+    ], ids=['int_case', 'bignum_case', 'float_case', 'minus_case', 'zero_case',
+    	"zero_int_case", "minus_int_case", "minus_bignum_case", "minus_bignum_int_case"
+    ])
     def test_add(self, a, b, expect):
         # calc = Calculator()
         result = self.calc.add(a, b)
         assert result == expect
 
-    def test_add1(self):
-        test_data = [
-            [1, 1, 2], [100, 1, 200], [0.1, 0.1, 0.2], [-1, -1, -2],
-            [1, 0, 1]]
-        for i in range(0, len(test_data)):
-            # calc = Calculator()
-            result = self.calc.add(test_data[i][0], test_data[i][1])
-            assert result == test_data[i][2]
+    @pytest.mark.parametrize('a, b, expect',[
+	[1, 1, 1], [100, 2, 50], [0.8, 2, 0.4], [1.2, 0.1, 12], [-100, -2, 50], [10000, 100, 100],
+	[15, 2, 7.5], [0, 10, 0]
+    ], ids=[ 'int_case', 'int_bignum_case', 'float_case', 'float_float_case', 'minus_case', 'bignum_case',
+    	'int_folat_case', 'zero_case'
+    ])
+    def test_div(self, a, b, expect):
+          result = self.calc.div(a, b)
+          assert result == expect
