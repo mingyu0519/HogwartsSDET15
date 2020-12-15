10076101f7fc96d6f7742178a43ced92e09b7a3b
mingyu.wang <mingyu.wang@ly.com>
2020-12-15 21:25:29 +0800

    接口测试 作业 完成新增，删除和清理标签功能

--- testing/interface/tag.py
+++ testing/interface/tag.py
@@ -2,12 +2,13 @@ import datetime
 import json
 
 import requests
+from jsonpath import jsonpath
 
 
 class Tag:
 
     def __init__(self):
-        self.token=self.get_token()
+        self.token = self.get_token()
 
     def get_token(self):
         corpid = 'wxaa6e18ac7322dc08'
@@ -25,13 +26,21 @@ class Tag:
         token = r.json()['access_token']
         return token
 
-    def add(self):
-        r = requests.post(
-            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list',
-            params={"access_token": self.token},
-            json={
-                'tag_id': []
+    def add(self, group_id, tag_name, group_name):
+        data = {
+                "tag": [{
+                    "name": tag_name
+                }
+                ]
             }
+        if group_id != '':
+            data['group_id'] = group_id
+        if group_name != '':
+            data['group_name'] = group_name
+        r = requests.post(
+            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag',
+            params={"access_token": self.token},
+            json=data
         )
 
         print(json.dumps(r.json(), indent=2))
@@ -61,5 +70,22 @@ class Tag:
         print(json.dumps(r.json(), indent=2))
         return r
 
-    def delete(self):
-        pass
+    def delete(self, tag_ids, group_ids=[]):
+        r = requests.post(
+            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag',
+            params={"access_token": self.token},
+            json={
+                "tag_id": tag_ids,
+                "group_id": group_ids
+            }
+        )
+        print(json.dumps(r.json(), indent=2))
+        return r
+
+    def clear(self):
+        r = self.list()
+        group_ids = jsonpath(r.json(), f"$..tag_group[*].group_id")
+        tag_ids = jsonpath(r.json(), f"$..tag_group[*].tag[*].id")
+        self.delete(tag_ids, group_ids)
+        r = self.list()
+        return r
--- testing/interface/test_tag.py
+++ testing/interface/test_tag.py
@@ -1,15 +1,8 @@
 import datetime
 import json
-
 import pytest
 import requests
-
-# done: 与底层具体的实现框架代码耦合严重，无法适应变化，比如公司切换了协议，比如使用pb dubbo
-# done: 代码冗余，需要封装
-# done: 无法清晰的描述业务
-# done: 使用jsonpath表达更灵活的递归查找
 from jsonpath import jsonpath
-
 from testing.interface.tag import Tag
 
 
@@ -17,49 +10,53 @@ class TestTag:
     def setup_class(self):
         self.tag = Tag()
 
-    @pytest.mark.parametrize("tag_id, tag_name", [
-        ['etQKd-CgAAqu3dqyzDrfEyrHb0lMmaJQ', 'tag1_new_'],
-        ['etQKd-CgAAqu3dqyzDrfEyrHb0lMmaJQ', 'tag1——中文'],
-        ['etQKd-CgAAqu3dqyzDrfEyrHb0lMmaJQ', 'tag1[中文]'],
+    # 增加标签-已有标签组
+    @pytest.mark.parametrize("group_id, tag_name", [
+        ['etEsuJCQAAckPHnumB9-Kyqrucop-sXQ', 'tag1_new_'],
+        ['etEsuJCQAAckPHnumB9-Kyqrucop-sXQ', 'tag1——中文'],
+        ['etEsuJCQAAckPHnumB9-Kyqrucop-sXQ', 'tag1[中文]']
     ])
-    def test_tag_list(self, tag_id, tag_name):
+    def test_tag_add(self, group_id, tag_name):
         tag_name = tag_name + str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
         r = self.tag.list()
-        r = self.tag.update(
-            id=tag_id,
+        r = self.tag.add(
+            group_id=group_id,
             tag_name=tag_name
         )
         r = self.tag.list()
-        # tags = [
-        #     tag
-        #     for group in r.json()['tag_group'] if group['group_name'] == group_name
-        #     for tag in group['tag'] if tag['name'] == tag_name
-        # ]
-
         assert jsonpath(r.json(), f"$..[?(@.name=='{tag_name}')]")[0]['name'] == tag_name
-        # assert tags != []
 
-    @pytest.mark.parametrize("tag_id, tag_name", [
-        ['etQKd-CgAAqu3dqyzDrfEyrHb0lMmaJQ', 'tag1_new_'],
-        ['etQKd-CgAAqu3dqyzDrfEyrHb0lMmaJQ', 'tag1——中文'],
-        ['etQKd-CgAAqu3dqyzDrfEyrHb0lMmaJQ', 'tag1[中文]'],
+    # 增加标签-无标签组
+    @pytest.mark.parametrize("group_id, tag_name, group_name", [
+        ['', 'tag1_new_', '测试标签组'],
+        ['', 'tag1——中文', '测试标签组'],
+        ['', 'tag1[中文]', '测试标签组']
     ])
-    def test_tag_add(self, tag_id, tag_name):
+    def test_tag_add_notag(self, group_id, tag_name, group_name):
         tag_name = tag_name + str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
         r = self.tag.list()
-        r = self.tag.update(
-            id=tag_id,
-            tag_name=tag_name
+        r = self.tag.add(
+            group_id=group_id,
+            tag_name=tag_name,
+            group_name=group_name
         )
         r = self.tag.list()
-        # tags = [
-        #     tag
-        #     for group in r.json()['tag_group'] if group['group_name'] == group_name
-        #     for tag in group['tag'] if tag['name'] == tag_name
-        # ]
-
         assert jsonpath(r.json(), f"$..[?(@.name=='{tag_name}')]")[0]['name'] == tag_name
-        # assert tags != []
 
-    def test_tag_list_fail(self):
-        pass
+    # 删除标签
+    def test_tag_del(self):
+        r = self.tag.list()
+        tag_ids = jsonpath(r.json(), f"$..tag_group[*].tag[*].id")
+        if len(tag_ids) == 0:
+            assert False, '当前没有可删除的标签'
+        tag = tag_ids[0]
+        r = self.tag.delete(
+            tag_ids=[tag]
+        )
+        r = self.tag.list()
+        assert tag not in jsonpath(r.json(), f"$..tag_group[*].tag[*].id")
+
+    # 标签数据清理
+    def test_tag_clean(self):
+        r = self.tag.clear()
+        assert r['tag_group'] == []
